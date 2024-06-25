cb8ba7bfc7532532c43d80af9623e2ecaf8c88fb
Anchit Sinha <anchit1909@gmail.com>
2024-06-26 02:30:48 +0530

    feat[backend]: add property and show property

--- backend/djangobnb_backend/useraccount/models.py
+++ backend/djangobnb_backend/useraccount/models.py
@@ -9,7 +9,7 @@ class CustomUserManager(UserManager):
     def _create_user(self, name, email, password, **extra_fields):
         if not email:
             raise ValueError("You have not specified a valid e-mail address")
-
+    
         email = self.normalize_email(email)
         user = self.model(email=email, name=name, **extra_fields)
         user.set_password(password)
@@ -21,7 +21,7 @@ class CustomUserManager(UserManager):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(name, email, password, **extra_fields)
-
+    
     def create_superuser(self, name=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -46,3 +46,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = ['name',]
+
+    def avatar_url(self):
+        if self.avatar:
+            return f'{settings.WEBSITE_URL}{self.avatar.url}'
+        else:
+            return ''
