8da9e26e90097b2da599638725758b65ba7936ed
Anchit Sinha <anchit1909@gmail.com>
2024-06-28 18:11:58 +0530

    build[backend]: add CSRF origins

--- backend/djangobnb_backend/djangobnb_backend/settings.py
+++ backend/djangobnb_backend/djangobnb_backend/settings.py
@@ -86,7 +86,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://139.59.75.6:1337',
 ]
 
-CORS_TRUSTED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
     'http://139.59.75.6',
