c2c8dc63a16f6673dda19bfbe25628d666827d45
Anchit Sinha <anchit1909@gmail.com>
2024-07-05 02:36:28 +0530

    style: change theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        airbnb: "#ff385c",
-        "airbnb-dark": "#d50027",
+        airbnb: "#f97316",
+        "airbnb-dark": "#ea580c",
       },
     },
   },
